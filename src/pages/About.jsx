5adea265fbc669777ddb51efdf667bec2e9a8bca
elshera dahlan <elshera.dahlan@gmail.com>
2024-11-19 20:17:38 +0800

    changing files

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
             <div className='grid grid-cols-2 justify-center h-[1000px] items-center max-[1505px]:grid-cols-1'>
                 <div className='text-start grid justify-center items-center'>
                     <h1 className='m-5 text-[27px] font-bold'>Our Story</h1>
-                    <p className='m-5'>Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense <br />in Bangladesh. Supported by wide range of tailored marketing, data and service solutions,<br /> Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. </p>
-                    <p className='m-5'>Exclusive has more than 1 Million products to offer, growing at a very fast. <br /> Exclusive offers a diverse assotment in categories ranging  from consumer.</p>
+                    <p className='m-5'>Launched in 2024, UNKLAB SHOP has been successfuly running with such a great income, UNKLAB Shop provides your need in just one click and it's close to your dormitory!,<br /> Exclusive has 1000 sells accross all UNKLAB. </p>
+                    <p className='m-5'>Exclusive has more than 50 products to offer, growing at a very fast. <br /> Exclusive offers a diverse assotment in categories ranging  from consumer.</p>
                 </div>
                 <div className='flex justify-center'>
                     <img className='h-[500px] max-[748px]:h-[400px] rounded-lg' src="/images/banner-unklab.jpg" alt="" />
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -68,18 +68,18 @@ const Contact = () => {
                         <div className='grid gap-3'>
                             <div className='flex gap-3 items-center'>
                                 <svg className="h-6 w-7 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" /></svg>
-                                <p>+1012 3456 789</p>
+                                <p>+62 89755748943</p>
                             </div>
                             <div className='flex gap-3 items-center'>
                                 <svg className="h-6 w-7 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />  <polyline points="22,6 12,13 2,6" /></svg>
-                                <p>demo@gmail.com</p>
+                                <p>unklabshop@gmail.com</p>
                             </div>
                             <div className='flex gap-3 items-center'>
                                 <svg className="h-6 w-7 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
-                                <p>132 Dartmouth Street Boston, Massachusetts 02156 United States</p>
+                                <p>Universitas Klabat, Airmadidi, Sulawesi Utara</p>
                             </div>
                         </div>
                         <div className='flex gap-5'>

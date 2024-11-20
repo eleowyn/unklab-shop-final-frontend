22363e14851484bb9dd3f0bc5ae0b758cfe27b06
Lana Londah <S22310372@student.unklab.ac.id>
2024-11-21 04:50:54 +0800

    firebase about us base 64 pic try lelah T-T

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import database from '../firebase-config'; // Sesuaikan path sesuai kebutuhan
+import { ref, onValue } from 'firebase/database';
 
 const About = () => {
-    return ( 
+    const [content, setContent] = useState({ par1: '', par2: '', img: '' });
+
+    useEffect(() => {
+        const contentRef = ref(database, 'aboutUs');
+        onValue(contentRef, (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                setContent({
+                    par1: data.par1,
+                    par2: data.par2,
+                    img: data.img, // Ambil data img
+                });
+            }
+        });
+    }, []);
+
+    return (
         <>
             <div className='grid grid-cols-2 justify-center h-[1000px] items-center max-[1505px]:grid-cols-1'>
                 <div className='text-start grid justify-center items-center'>
                     <h1 className='m-5 text-[27px] font-bold'>Our Story</h1>
-                    <p className='m-5'>Launched in 2024, UNKLAB SHOP has been successfuly running with such a great income, UNKLAB Shop provides your need in just one click and it's close to your dormitory!,<br /> Exclusive has 1000 sells accross all UNKLAB. </p>
-                    <p className='m-5'>Exclusive has more than 50 products to offer, growing at a very fast. <br /> Exclusive offers a diverse assotment in categories ranging  from consumer.</p>
+                    <p className='m-5'>{content.par1}</p>
+                    <p className='m-5'>{content.par2}</p>
                 </div>
                 <div className='flex justify-center'>
-                    <img className='h-[500px] max-[748px]:h-[400px] rounded-lg' src="/images/banner-unklab.jpg" alt="" />
+                    {content.img ? (
+                        <img 
+                            className='h-[500px] max-[748px]:h-[400px] rounded-lg' 
+                            src={content.img} 
+                            alt="Dynamic Image" 
+                        />
+                    ) : (
+                        <p>Loading image...</p>
+                    )}
                 </div>
             </div>
         </>

e3ecdd1d58feb63c428c831f84b87e90452472b4
elshera dahlan <elshera.dahlan@gmail.com>
2024-11-21 00:41:31 +0800

    adding intergrations to all items for products items into firebase

--- src/pages/AllItems.jsx
+++ src/pages/AllItems.jsx
@@ -1,17 +1,33 @@
 // Product.js
 import * as React from 'react';
 import { UserProvider } from '../Context/CartContext';
-import itemss from '../data/itemss.json';
 import { useParams } from 'react-router-dom'; // Import useParams hook
 import ItemForAllItems from '../components/ItemForAllItems';
+import { getDatabase, ref, child, get } from 'firebase/database';
+
 const AllItems = () => {
-    // Method 3: window.location
-    // const id = window.location.pathname.slice(-1);
     const {ItemtId} = useParams()
+    const dbRef = ref(getDatabase());
+    const itemsRef = child(dbRef, 'products');
+
+    const [items, setItems] = React.useState([]);
+
+    React.useEffect(() => {
+        get(itemsRef).then((snapshot) => {
+            if (snapshot.exists()) {
+                setItems(Object.values(snapshot.val()));
+            } else {
+                console.log('No data available');
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }, []);
+ 
 
     return (
         <>
-            {itemss.map((item) => (
+            {items.map((item) => (
                 // Only render the item that matches the id
                 item.id == ItemtId && (
                     <div key={ItemtId}>

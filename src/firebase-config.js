2fbe9c43c556cd6f28b7f8bf494a18fd11efad1f
Lana Londah <S22310372@student.unklab.ac.id>
2024-11-20 22:55:53 +0800

    adding new branch, firebase offer integration

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,24 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// src/firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDep1iPXBfPMO3taeiZXIhHKwmqoFDJaFQ",
-  authDomain: "unklab-shop-final-project.firebaseapp.com",
-  databaseURL: "https://console.firebase.google.com/u/2/project/unklab-shop-final-project/database/unklab-shop-final-project-default-rtdb/data/~2F",
-  databaseURL: "https://unklab-shop-final-project-default-rtdb.firebaseio.com",
-  projectId: "unklab-shop-final-project",
-  storageBucket: "unklab-shop-final-project.firebasestorage.app",
-  messagingSenderId: "102194537251",
-  appId: "1:102194537251:web:d653b5f11fb6ef5da72296",
-  measurementId: "G-KP126E0J0G"
+  apiKey: "AIzaSyAiMWWM4gyoNW8-ZatnIGmTIl66dMEWi9U",
+  authDomain: "final-project-front-end-7fde3.firebaseapp.com",
+  databaseURL: "https://final-project-front-end-7fde3-default-rtdb.firebaseio.com",
+  projectId: "final-project-front-end-7fde3",
+  storageBucket: "final-project-front-end-7fde3.firebasestorage.app",
+  messagingSenderId: "579070235405",
+  appId: "1:579070235405:web:91e49c47ee7fe1fcae5644"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
-// Initialize Realtime Database
-export const database = getDatabase(app);
+export { database };
